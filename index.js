656165e447e56588239993e3796668922d57395b
Ronney Arias <ronneyarias@gmail.com>
2023-10-13 12:44:01 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+
+const returnLastTwoDrivers = function(array) {
+  return array.slice(array.length - 2, array.length);
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+function createFareMultiplier(fareMultiplier) {
+
+    return function doubleMultiplier(fare){
+        return (fare * fareMultiplier);
+    }
+
+}
+function fareDoubler(fare) {
+    return fare * 2;
+  }
+
+function fareTripler(fare) {
+    return fare * 3;
+  }
+
+function selectDifferentDrivers(arrayOfDrivers, callBack) {
+    if (returnFirstTwoDrivers === callBack) {
+      return arrayOfDrivers.slice(0, 2);
+    } else if (returnLastTwoDrivers === callBack) {
+      return arrayOfDrivers.slice(-2);
+    
+    }} 
+    
